236437516a0e107ab74a84529ae354887abc5938
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-07-13 10:56:56 +0530

    Add feed pages and update layout and home
    Added new feed and videoId pages under /feed. Replaced Geist fonts with Inter in layout. Updated home page to display logo and title instead of button.

---
+++ src/app/feed/[videoId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = ({ params }) => {
+  return (
+    <div>Video Id {params.videoId}</div>
+  )
+}
+
+export default Page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Button variant="destructive">
-        Click me
-      </Button>
+     <Image src="logo.svg" alt="logo" height={50} width={50} />
+     <p className="text-xl font-semibold tracking-tight">NewTube</p>
     </div>
   )
 }

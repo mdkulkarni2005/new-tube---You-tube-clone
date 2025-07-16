1726a2e17d0fae227c4595396a32c0f99f145176
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-07-17 00:02:58 +0530

    Add search input to HomeNavbar component
    Introduced a new SearchInput component and integrated it into the HomeNavbar for improved navigation and user experience.

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -1,11 +1,13 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import Image from "next/image";
 import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 export const HomeNavbar = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
       <div className="flex items-center gap-4 w-full">
+        {/* Menu And logo */}
         <div className="flex items-center flex-shrink-0">
           <SidebarTrigger />
           <Link href="/">
@@ -14,6 +16,11 @@ export const HomeNavbar = () => {
               <p className="text-xl font-semibold tracking-tight">New Tube</p>
             </div>
           </Link>
+        </div>
+
+        {/* Search Bar */}
+        <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
+          <SearchInput />
         </div>
       </div>
     </nav>

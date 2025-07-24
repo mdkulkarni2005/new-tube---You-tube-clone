6b5ac6b6049a73d6820fc7e8f1a9518f1f22c50e
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-07-24 18:59:23 +0530

    Implement paginated video fetching in studio module
    Added a DEFAULT_LIMIT constant and updated the studioRouter's getMany procedure to support cursor-based pagination with limit. Updated the studio page and VideosSection component to use the new paginated API, enabling efficient infinite loading of videos for the current user.

--- src/modules/studio/ui/sections/videos-sections.tsx
+++ src/modules/studio/ui/sections/videos-sections.tsx
@@ -1,9 +1,19 @@
 "use client"
 
+import { DEFAULT_LIMIT } from "@/constants"
+import { trpc } from "@/trpc/client"
+
 export const VideosSection = () => {
+
+    const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
+        limit: DEFAULT_LIMIT
+    }, {
+        getNextPageParam: (lastPage) => lastPage.nextCursor
+    })
+
     return (
         <div>
-            Videos Section
+            {JSON.stringify(data)}
         </div>
     )
 }
